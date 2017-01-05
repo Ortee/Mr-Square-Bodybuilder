63b47b2b574a89fdfa13146498a399ca860a0b30
Mateusz <kontakt@mateuszoracz.pl>
2017-01-05 22:55:59 +0100

    added GET Gyms by location

--- Bodybuilder/StatsViewController.swift
+++ Bodybuilder/StatsViewController.swift
@@ -15,6 +15,34 @@ class StatsViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet var gpsX: UILabel!
     @IBOutlet var gpsY: UILabel!
     
+    func getNerbyGyms(latitude: Double, longitude: Double) -> [Gym]{
+        
+        let url = URL(string: "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(latitude),\(longitude)&radius=500&type=gym&keyword=silownia&key=AIzaSyDJo8nIHl3JbPGLmpyZbMA7PkQMZj_AeUw")
+        URLSession.shared.dataTask(with:url!, completionHandler: {(data, response, error) in
+            guard let data = data, error == nil else { return }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as! [String:Any]
+                let posts = json["results"] as? [[String: Any]] ?? []
+                var gymsArray: [Gym] = []
+                for gym in posts {
+                    gymsArray.append(
+                        Gym(
+                            nam: gym["name"] as! String,
+                            add: gym["vicinity"] as! String,
+                            lat: ((gym["geometry"] as! Dictionary<String, Any>)["location"] as! Dictionary<String, Double>)["lat"] as! Double!,
+                            long: ((gym["geometry"] as! Dictionary<String, Any>)["location"] as! Dictionary<String, Double>)["lng"] as! Double!
+                        )
+                    )
+                    
+                }
+                print(gymsArray)
+            } catch let error as NSError {
+                print(error)
+            }
+        }).resume()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager = CLLocationManager()
@@ -27,6 +55,10 @@ class StatsViewController: UIViewController, CLLocationManagerDelegate {
         super.didReceiveMemoryWarning()
     }
     
+    @IBAction func getGymsButton(_ sender: AnyObject) {
+        getNerbyGyms(latitude: 52.4388894, longitude: 16.9184323)
+        
+    }
     var location: CLLocation! {
         didSet {
             gpsX.text = String(location.coordinate.latitude)
