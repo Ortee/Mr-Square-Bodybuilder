953d196ed1ccdecd61bd47fcf8998fb25b6a3d01
Mateusz <kontakt@mateuszoracz.pl>
2017-01-05 21:19:34 +0100

    added class Gym

---
+++ Bodybuilder/gym.swift
@@ -0,0 +1,40 @@
+//
+//  gym.swift
+//  Bodybuilder
+//
+//  Created by Mateusz Oracz on 05.01.2017.
+//  Copyright © 2017 BatoregoTeam. All rights reserved.
+//
+
+import Foundation
+
+class Gym {
+    var latitude: Double
+    var longitude: Double
+    var name: String
+    var address: String
+    
+    init(nam: String, add: String, lat: Double, long: Double) {
+        name = nam
+        address = add
+        latitude = lat
+        longitude = long
+    }
+    
+    func getName() -> String {
+        return name
+    }
+    
+    func getAddress() -> String {
+        return address
+    }
+    
+    func getLatitude() -> Double {
+        return latitude
+    }
+    
+    func getLongitude() -> Double {
+        return longitude
+    }
+    
+}
