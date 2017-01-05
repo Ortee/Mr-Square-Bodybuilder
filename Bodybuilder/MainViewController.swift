6eeaad86af50ba2dc6b4ac87d78fd0b03ada16a5
Mateusz <kontakt@mateuszoracz.pl>
2017-01-05 02:43:20 +0100

    buttons & labels actions

---
+++ Bodybuilder/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  Bodybuilder
+//
+//  Created by Mateusz Oracz on 05.01.2017.
+//  Copyright © 2017 BatoregoTeam. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
--- Bodybuilder/StatsViewController.swift
+++ Bodybuilder/StatsViewController.swift
@@ -8,27 +8,26 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StatsViewController: UIViewController {
     
     var bodybuilder = (UIApplication.shared.delegate as! AppDelegate).bodybuilder
+
+    @IBOutlet weak var trainingValue: UILabel!
+    @IBOutlet weak var hungerValue: UILabel!
+    @IBOutlet weak var cashValue: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
     
-    @IBAction func training(_ sender: AnyObject) {
-        bodybuilder.increaseStrength()
-    }
-    @IBAction func eating(_ sender: AnyObject) {
-        bodybuilder.increaseHunger()
+    @IBAction func refresh(_ sender: AnyObject) {
+        trainingValue.text = String(bodybuilder.getStrength())
+        hungerValue.text = String(bodybuilder.getHunger())
+        cashValue.text = String(bodybuilder.getCash())
     }
     
-    @IBAction func working(_ sender: AnyObject) {
-        bodybuilder.increaseCash()
-    }
 }
